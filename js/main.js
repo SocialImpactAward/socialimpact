914ffe63bf462367111a79b7215fcebed0ef354e
SocialImpactAward <95848074+SocialImpactAward@users.noreply.github.com>
2021-12-09 09:31:01 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+  
+$(document).ready(funtion (){
+
+//sticky navigation menu
+
+let nav_offset_top = $('.header_area').height() + 50;
+
+function navbarFixed(){
+    if($('.header_area').length){
+        $(window).scroll(function(){
+            let scroll=$(window).scrollTop();
+            if(scroll>= nav_offset_top){
+                $('.header-area.main-menu').addClass('navbar_fixed');
+            }else{
+                $('.header-area.main-menu').removeClass('navbar_fixed');
+
+            }
+        })
+    }
+}
+
+navbarFixed();
+
+});
